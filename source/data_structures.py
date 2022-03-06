33898b0d4500a2bbf6d39044b31e981e73f1ff4a
Santiago del Rey <59149491+santidrj@users.noreply.github.com>
2022-03-06 16:41:05 +0100

    Finish implementation of cci step
    Signed-off-by: Santiago del Rey <59149491+santidrj@users.noreply.github.com>

---
+++ source/data_structures.py
@@ -0,0 +1,34 @@
+import heapq
+
+
+class MaxHeapObj:
+    """
+    Max-Heap object.
+
+    This class redefines the lower than operator in order to emulate the behavior of a Max-Heap using the heapq library.
+    """
+
+    def __init__(self, val): self.val = val
+
+    def __lt__(self, other): return self.val > other.val
+
+    def __eq__(self, other): return self.val == other.val
+
+    def __str__(self): return str(self.val)
+
+
+class MaxHeap(object):
+    """
+    Max-Heap data structure.
+    """
+
+    def __init__(self):
+        self.h = []
+
+    def heappush(self, x): heapq.heappush(self.h, MaxHeapObj(x))
+
+    def heappop(self): return heapq.heappop(self.h).val
+
+    def __getitem__(self, i): return self.h[i].val
+
+    def __len__(self): return len(self.h)
--- source/qbca.py
+++ source/qbca.py
@@ -1,7 +1,10 @@
-import heapq
+import copy
 import math
+from itertools import product
 
 import numpy as np
+
+from source.data_structures import MaxHeap
 
 
 class QBCA:
@@ -10,7 +13,7 @@ class QBCA:
 
     def fit(self, x):
         self._quantization(x)
-        self._cci(self.n_seeds)
+        self._cluster_center_initialization()
         pass
 
     def predict(self, x):
@@ -21,10 +24,17 @@ class QBCA:
 
     def _initialize_bins(self, x):
         self.m_dims = x.shape[-1]
+        self.min_values = np.min(x, axis=0)
         self.n_bins = math.floor(math.log(x.shape[0], self.m_dims))
         self.bin_size = (np.max(x, axis=0) - np.min(x, axis=0)) / self.n_bins
-        self.bins = np.zeros(self.n_bins ** self.m_dims, dtype=int)
-        self.min_values = np.min(x, axis=0)
+        self.bin_counts = np.zeros(self.n_bins ** self.m_dims, dtype=int)
+        self.bins = [[] for _ in range(self.n_bins ** self.m_dims)]
+        self.hist_shape = tuple([self.n_bins] * self.m_dims)
+
+        # Create a mask for all the possible positions of the neighbors wrt a point
+        aux_list = list(product([-1, 0, 1], repeat=self.m_dims))
+        aux_list.remove(tuple(np.zeros(self.m_dims)))
+        self.neighbors_mask = np.array(aux_list)
 
     def _quantize_point(self, point):
         epsilon = point.copy()
@@ -39,17 +49,58 @@ class QBCA:
     def _quantization(self, x):
         self._initialize_bins(x)
         idxs = np.apply_along_axis(self._quantize_point, axis=1, arr=x)
-        for idx in idxs:
-            self.bins[idxs] += 1
+        for idx, point in zip(idxs, x):
+            self.bin_counts[idx] += 1
+            self.bins[idx].append(point)
+
+    def _cluster_center_initialization(self):
+        # Create Max-Heap with the non-zero bins
+        non_zero_bins, non_zero_bins_idx = self._get_non_zero_bins()
+        max_heap = MaxHeap()
+        [max_heap.heappush((x[0], x[1])) for x in np.stack((non_zero_bins, non_zero_bins_idx)).T]
+        aux_heap = copy.copy(max_heap)
+        seed_list = []
+
+        while len(max_heap) > 0:
+            b = max_heap.heappop()
+            flag = True
+            neighbors = self._get_neighbors(b[1])
+
+            if (self.bin_counts[neighbors] <= b[0]).all():
+                seed_list.append(b)
+
+        # If there are not enough seeds
+        if len(seed_list) < self.n_seeds:
+            seed_list.extend([x for x in aux_heap if x not in seed_list][: self.n_seeds - len(seed_list)])
+        seed_list.sort(key=self._bin_cardinality)
+
+        self.seeds = np.zeros((self.n_seeds, self.m_dims))
+        for s_idx, (_, b_idx) in enumerate(seed_list[:self.n_seeds]):
+            self.seeds[s_idx] = self._compute_center(self.bins[b_idx])
 
     def _get_non_zero_bins(self):
-        non_zero_bins = np.where(self.bins != 0)
-        return self.bins[non_zero_bins], non_zero_bins[0]
+        non_zero_bins = np.where(self.bin_counts != 0)
+        return self.bin_counts[non_zero_bins], non_zero_bins[0]
 
-    def _cci(self, n_seeds):
-        seed_list = np.zeros((n_seeds, self.m_dims))
-        non_zero_bins, non_zero_bins_idx = self._get_non_zero_bins()
+    def _bin_cardinality(self, bin: tuple):
+        return bin[0]
 
-        # Negate non_zero_bins values to simulate a max_heap
-        max_heap = np.stack((-non_zero_bins, non_zero_bins_idx)).T
-        heapq.heapify(max_heap.tolist())
+    def _get_neighbors(self, index):
+        """
+        Given a bin index return all its neighbors indexes.
+
+        :param index: The bin index.
+        :return: An ndarray of the neighbors indexes.
+        """
+        coord = np.unravel_index(index, self.hist_shape)
+        neighbors = self.neighbors_mask + coord
+        # Remove coordinates that are out of the histogram
+        neighbors = neighbors[~(neighbors < 0).any(1)]
+        neighbors = neighbors[~(neighbors >= self.n_bins).any(1)]
+        neighbors_idx = []
+        for n in neighbors:
+            neighbors_idx.append(np.ravel_multi_index(n, self.hist_shape))
+        return np.array(neighbors_idx)
+
+    def _compute_center(self, cluster):
+        return np.mean(np.array(cluster), axis=0)
